c2729ba33ff0f9a4f18392e7cd5f437ed4120119
Cleiton <cleitonman@outlook.com>
2020-09-18 09:59:56 -0300

    Autenticação

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ Route::get('panel', 'Panel\PanelController@index');
 
 Route::get('/', 'Site\SiteController@index');
 Route::get('promocoes', 'Site\SiteController@promotions')->name('promotions');
+
+Auth::routes();
